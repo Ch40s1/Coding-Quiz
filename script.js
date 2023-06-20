3d6a9c5f05067ea0401949a8967f844898fdc0ba
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-06-20 13:13:34 -0500

    made a pseudocode for quiz

---
+++ script.js
@@ -0,0 +1,17 @@
+// make array with q and a 
+
+// assign q and a to designated box 
+
+// when aswers is clicked show answer right or wrong 
+
+// if answer is wrong deduct Time 
+
+// if answer is right add points 
+
+// when question is answered move onto next question
+
+// if question is at the end move to leaderboard screen
+
+// put initials in a box and submit to the leaderboard 
+
+// game can be restarted 
