03f835d2998a56e24658a1c2e79db313e16b7271
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-06-23 11:39:59 -0500

    made script for quiz page, and deleted some css

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-// make array with q and a 
+// load quiz //
+// const startBtn = document.querySelector("#start-btn");
 
+// //click start and go to quiz html
+// startBtn.addEventListener("click", () => {
+//   window.location.href = "./html-pages/quiz.html";
+// });
+
+//setting up the quiz///
+
+// make a object with q and a 
+const quizIndex = [
+  {
+    question: "how are you ?",
+    answerIndex: [ 
+      {answer: "lol!", correct: true},
+      {answer: "happy!", correct: false},
+      {answer: "sadge", correct: false},
+      {amswer: "bruh", correct: false}
+    ]
+  }
+]
 // assign q and a to designated box 
-
+const questionBox = document.querySelector(".question");
+// const answerBox = document.querySelector(".answerBtn");
+// questionBox.textContent = quizIndex[0].question;
+// answerBox.textContent = quizIndex[0].answerIndex[1].answer;
+let currentQuestion = quizIndex[0].question;
+questionBox.textContent = currentQuestion;
+const answerBoxes = document.querySelectorAll(".answerBtn");
+let currentAnswer = quizIndex[0].answerIndex[0].answer;
+answerBoxes[0].textContent = currentAnswer;
+// let answerIndexLength = quizIndex[0].answerIndex.length;
+// console.log(answerIndexLength);
+// let answerContLenth = answerCont.length;
+// console.log(answerContLenth);
+/* when it is answerCont 0 then answerIndex should also be 0. quiz Index is the same.*/
+// function x(){
+//   let y = 0;
+//   let z = 0;
+// }
 // when aswers is clicked show answer right or wrong 
 
 // if answer is wrong deduct Time 
